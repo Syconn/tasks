098d4dcdeee80f12cfc4d0e74c3b1ff830dd9429
Aidan Haack <49461127+Syconn@users.noreply.github.com>
2026-03-09 09:55:12 -0400

    Checking Answer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,14 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [answer, setAnswer] = useState("");
+
     return (
         <div>
             <h3>Check Answer</h3>
+
+            <input onChange={e => { setAnswer(e.target.value) }} value={answer} />
+            {answer === expectedAnswer ? "✔️" : "❌"}
         </div>
     );
 }
