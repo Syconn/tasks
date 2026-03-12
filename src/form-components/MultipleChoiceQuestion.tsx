78b1f092b1ce159ea96f6e9e03a75f6ba467af37
Aidan Haack <49461127+Syconn@users.noreply.github.com>
2026-03-12 12:22:39 -0400

    Solved multiple choice

--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function MultipleChoiceQuestion({
     options,
@@ -7,9 +8,23 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [selectedAnswer, setSelectedAnswer] = useState<string>(options[0])
+
+    function updateAnswer(event: React.ChangeEvent<HTMLSelectElement>) {
+        setSelectedAnswer(event.target.value);
+    }
+
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+
+            <Form.Select value={selectedAnswer} onChange={updateAnswer}>
+                { options.map((answer: string) =>
+                    <option key={answer} value={answer}>{answer}</option>
+                )}
+            </Form.Select>
+
+            <span>Your Answer is {selectedAnswer === expectedAnswer ? "✔️" : "❌"}</span>
         </div>
     );
 }
