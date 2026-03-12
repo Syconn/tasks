6751528c2e7c6eb77f323c70f4ca1ce8c4619310
Aidan Haack <49461127+Syconn@users.noreply.github.com>
2026-03-12 12:14:31 -0400

    Finished EditMode and Change Color

--- src/form-components/ChangeColor.tsx
+++ src/form-components/ChangeColor.tsx
@@ -1,9 +1,33 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function ChangeColor(): React.JSX.Element {
+    const [color, setColor] = useState<string>("red");
+
+    const colors = ["red", "orange", "green", "blue", "yellow", "purple", "pink", "black"]
+
     return (
         <div>
             <h3>Change Color</h3>
+            <div>
+                {colors.map((thisColor: string) => (
+                    <Form.Check
+                        inline={true}
+                        key={thisColor}
+                        type="radio"
+                        onChange={() => { setColor(thisColor) }}
+                        id="emotion-check-happy"
+                        label={thisColor}
+                        value={color}
+                        checked={color === thisColor}
+                        style={{ backgroundColor: thisColor }}
+                    />
+                ))}
+            </div>
+            <span>
+                You have chosen
+                <span data-testid="colored-box" style={{ backgroundColor: color }}> {color}</span>
+            </span>
         </div>
     );
 }
--- src/form-components/EditMode.tsx
+++ src/form-components/EditMode.tsx
@@ -1,9 +1,48 @@
 import React, { useState } from "react";
+import { Form } from "react-bootstrap";
 
 export function EditMode(): React.JSX.Element {
+    const [editMode, setEditMode] = useState<boolean>(false)
+    const [name, setName] = useState("Your Name");
+    const [isStudent, setIsStudent] = useState(true);
+
     return (
         <div>
-            <h3>Edit Mode</h3>
+            <Form.Check
+                type="switch"
+                label="Edit Mode"
+                checked={editMode}
+                onChange={() => { setEditMode(!editMode) }}
+            />
+
+            {!editMode && (
+                <p>
+                    {name} is {isStudent ? "a student" : "not a student"}
+                </p>
+            )}
+
+            {editMode && (
+                <div>
+                    <label>
+                        Name:
+                        <input
+                            type="text"
+                            value={name}
+                            onChange={(e) => { setName(e.target.value) }}
+                        />
+                    </label>
+
+                    <label>
+                        Student:
+                        <input
+                            type="checkbox"
+                            checked={isStudent}
+                            onChange={(e) => { setIsStudent(e.target.checked) }}
+                        />
+                    </label>
+                </div>
+            )}
+
         </div>
     );
 }
